5d2a9a1453dc78d183ce2d2556e9d4abf27a0c0b
Arslan23 <expeditaza@gmail.com>
2022-10-02 13:51:07 +0100

    Refactoring monolithique to microservice

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,7 +1,6 @@
 import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
-
 export const sequelize = new Sequelize({
   'username': config.username,
   'password': config.password,
@@ -11,3 +10,4 @@ export const sequelize = new Sequelize({
   'dialect': config.dialect,
   'storage': ':memory:',
 });
+
