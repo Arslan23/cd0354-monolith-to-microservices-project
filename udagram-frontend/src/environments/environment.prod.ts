bd2326d97b0dca671604d9cd1e0a0ec2c76b0567
Arslan23 <expeditaza@gmail.com>
2022-10-02 19:53:48 +0100

    Update frontend with reverse proxy load balancer

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab0a9542d75ea4d3ab7504892a95e4a0-176006398.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
